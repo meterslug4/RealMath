b69046f09618529e827e8fd1e64709af01430694
sjj324 <yongjae.link@gmail.com>
2020-07-15 10:15:41 +0900

    용제 씬 추가

---
+++ Assets/Scripts/TimeBarControl.cs
@@ -0,0 +1,62 @@
+﻿using GlowingSwords.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeBarControl : MonoBehaviour
+{
+    public float playTimeCurrent = 10.0f;
+    public float playTimeMax = 10.0f;
+
+    public Image TimeBar;
+    public GameObject dieEffect;
+    public GameObject robot;
+
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        SkinnedMeshRenderer render = robot.GetComponent<SkinnedMeshRenderer>();
+        Material mat = render.material;
+        time += Time.deltaTime * 0.2f;
+        Color goRed = Color.Lerp(Color.blue, Color.red, time);
+        mat.SetColor("_Color", goRed);
+
+        playTimeCurrent -= 1 * Time.deltaTime;
+
+        TimeBar.fillAmount = playTimeCurrent / playTimeMax;
+
+        if (playTimeCurrent < 5 && playTimeCurrent > 0)
+        {
+            gameObject.GetComponent<Animator>().SetBool("TakeDamage", true);
+        }
+        else if (playTimeCurrent < 0)
+        {
+
+            playTimeCurrent = 0;
+            Instantiate(dieEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    public void MissionSuccess()
+    {
+        if(playTimeCurrent < 7)
+        {
+           playTimeCurrent += 3;
+
+        }
+        else
+        {
+            playTimeCurrent = playTimeMax;
+        }
+    }
+
+}
